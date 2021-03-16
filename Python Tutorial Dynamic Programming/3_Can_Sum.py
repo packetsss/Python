59497fc9d067eccf5ccdd832ac6aa4f21ffd4957
Paul Pan <pyjpanyanjunpyj@gmail.com>
2021-03-15 22:40:32 -0700

    Updated Dynamic programming

---
+++ Python Tutorial Dynamic Programming/3_Can_Sum.py
@@ -0,0 +1,104 @@
+from timeit import default_timer as timer
+
+
+"""
+given a target sum n and a list numbers, return True if numbers in the list can sum up to the target sum
+
+(7, [2, 4]) --> False
+(7, [2, 3]) --> True
+
+"""
+
+def canSum(target, numbers):
+    if target == 0:
+        return True
+    elif target < 0:
+        return False
+
+    for i in numbers:
+        reminder = target - i
+        if canSum(reminder, numbers):
+            return True
+    return False
+
+
+start = timer()
+print(canSum(91, [6, 3]))
+end = timer()
+print(f"Naive time: {end - start}")
+# very slow
+
+
+### Memoized
+def canSum_d(target, numbers, d=None):
+    if d is None:
+        d = {}
+    if target == 0:
+        return True
+    elif target < 0:
+        return False
+    elif target in d:
+        return d[target]
+
+    for i in numbers:
+        reminder = target - i
+        d[target] = canSum_d(reminder, numbers, d)
+        if d[target]:
+            return True
+    return False
+
+
+start = timer()
+print(canSum_d(1121, [6, 7]))
+end = timer()
+print(f"Dynamic time: {end - start}")
+# slower
+
+def canSum_d2(target, numbers, d=None):
+    if d is None:
+        d = {}
+    if target == 0:
+        return True
+    elif target < 0:
+        return False
+    elif target in d:
+        return d[target]
+
+    for i in numbers:
+        reminder = target - i
+
+        if canSum_d(reminder, numbers, d):
+            d[target] = True
+            return True
+    d[target] = False
+    return False
+
+
+start = timer()
+print(canSum_d2(1121, [6, 7]))
+end = timer()
+print(f"Dynamic time: {end - start}")
+# fastest
+
+
+### Tabulation
+def canSum_botup(tar, nums):
+
+    lst = [False] * (tar + 1)
+    lst[0] = True
+    for i in range(tar):
+        if lst[i]:
+            nums = [num for num in nums if i + num <= tar]
+
+            for num in nums:
+                lst[i + num] = True
+        if lst[-1]:
+            return True
+    return False
+
+
+start = timer()
+print(canSum_botup(1121, [6, 7]))
+end = timer()
+print(f"Dynamic time: {end - start}")
+
---
+++ Python Tutorial Dynamic Programming/4_How_Sum.py
@@ -0,0 +1,68 @@
+from timeit import default_timer as timer
+
+"""
+given a target n and a list numbers, return 1 combination of elements that add up to exactly target
+
+(7, [2, 4]) --> Null
+(7, [2, 3]) --> [2, 2, 3]
+(7, [5, 3, 4, 7]) --> [7]
+
+                7
+       /     /     \    \
+      2     4       3    0
+           / \       |
+          1   0      0
+
+"""
+
+def howSum(tar, nums):
+    if not tar:
+        return []
+    elif tar < 0:
+        return None
+
+    for num in nums:
+        rem = tar - num
+        rst = howSum(rem, nums)
+        if rst is not None:
+            return rst + [num]
+    return None
+
+
+start = timer()
+print(howSum(92, [6, 3]))
+end = timer()
+print(f"Naive time: {end - start}")
+# n --> tar, n --> lst length
+# Time: O(n^m * m)
+
+def howSum_d(tar, nums, d=None):
+    if d is None:
+        d = {}
+    elif tar in d:
+        return d[tar]
+    # return memorized value instead of recalculating
+    elif not tar:
+        return []
+    elif tar < 0:
+        return None
+
+    for num in nums:
+        rem = tar - num
+        rst = howSum_d(rem, nums, d)
+        if rst is not None:
+            d[tar] = rst + [num]
+            # memorize it
+            return d[tar]
+    d[tar] = None
+    return None
+
+
+start = timer()
+print(howSum_d(300, [7, 14]))
+end = timer()
+print(f"Naive time: {end - start}")
+# Time: O(n * m^2)
+
+
+
