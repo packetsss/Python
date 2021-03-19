f5bbc6f58b516aabbbba2a00920b49fb46794c4a
Paul Pan <pyjpanyanjunpyj@gmail.com>
2021-03-18 21:09:23 -0700

    updated more PyQt5 stuff!!!

---
+++ Python Tutorial PyQt5/17_Printer.py
@@ -0,0 +1,66 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtPrintSupport import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(866, 590)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textEdit = QtWidgets.QTextEdit(Form)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.print_file)
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.print_preview_dialog)
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Print File"))
+        self.pushButton_2.setText(_translate("Form", "Print Preview"))
+
+    def print_file(self):
+        printer = QPrinter(QPrinter.HighResolution)
+        dialog = QPrintDialog(printer, self)
+
+        if dialog.exec_() == QPrintDialog.Accepted:
+            self.textEdit.print_(printer)
+
+    def print_preview_dialog(self):
+        printer = QPrinter(QPrinter.HighResolution)
+        pre = QPrintPreviewDialog(printer, self)
+        pre.paintRequested.connect(self.print_preview)
+        pre.exec_()
+
+    def print_preview(self, printer):
+        self.textEdit.print_(printer)
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
---
+++ Python Tutorial PyQt5/18_List_widget.py
@@ -0,0 +1,95 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(938, 593)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        Form.setFont(font)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.listWidget = QtWidgets.QListWidget(Form)
+        font = QtGui.QFont()
+        font.setFamily("MS Shell Dlg 2")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.listWidget.setFont(font)
+        self.listWidget.setStyleSheet("QListWidget{\n"
+                                      "    color: rgb(170, 85, 255);\n"
+                                      "    font: 75 16pt \"MS Shell Dlg 2\";\n"
+                                      "    background-color: rgb(197, 255, 255);\n"
+                                      "}")
+        self.listWidget.setObjectName("listWidget")
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        self.listWidget.clicked.connect(self.item_clicked)
+        
+        self.verticalLayout.addWidget(self.listWidget)
+        self.label = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet("QLabel{\n"
+                                 "color:rgb(255, 85, 0)\n"
+                                 "}")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "List"))
+        __sortingEnabled = self.listWidget.isSortingEnabled()
+        self.listWidget.setSortingEnabled(False)
+        item = self.listWidget.item(0)
+        item.setText(_translate("Form", "Python"))
+        item = self.listWidget.item(1)
+        item.setText(_translate("Form", "Java"))
+        item = self.listWidget.item(2)
+        item.setText(_translate("Form", "C++"))
+        item = self.listWidget.item(3)
+        item.setText(_translate("Form", "C#"))
+        item = self.listWidget.item(4)
+        item.setText(_translate("Form", "JavaScript"))
+        item = self.listWidget.item(5)
+        item.setText(_translate("Form", "Kotlin"))
+        self.listWidget.setSortingEnabled(__sortingEnabled)
+
+    def item_clicked(self):
+        item = self.listWidget.currentItem()
+        self.label.setText(f"You have selected: {item.text()}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
