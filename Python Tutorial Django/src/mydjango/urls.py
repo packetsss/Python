934570d1523cc456ac3e4ed74c8f56b19e1a17dc
Paul Pan <pyjpanyanjunpyj@gmail.com>
2021-03-23 12:13:02 -0700

    updated new stuff

--- Python Tutorial Django/src/mydjango/urls.py
+++ Python Tutorial Django/src/mydjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view
 
 urlpatterns = [
+    path("", home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
