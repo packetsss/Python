33022360c2fbf7b1cd100500a6583e5ebe31e844
Paul Pan <pyjpanyanjunpyj@gmail.com>
2021-03-23 19:28:17 -0700

    updated

--- Python Tutorial Django/src/mydjango/settings.py
+++ Python Tutorial Django/src/mydjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'mydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],  # good way to get templates dir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
