60d0147c6f715a9d7ebdf955b1ef5b85f376876b
Paul Pan <76851428+packetsss@users.noreply.github.com>
2021-06-09 11:57:39 -0700

    Update 1_hand_tracking_minimum.py

--- Python Tutorial Computer Vision/1_hand_tracking_minimum.py
+++ Python Tutorial Computer Vision/1_hand_tracking_minimum.py
@@ -3,11 +3,17 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(1)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+mpHands = mp.solutions.hands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while 1:
     success, img = cap.read()
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    rst = mpHands.process(rgb)
+    if (hands := rst.multi_hand_landmarks) is not None:
+        for hand in hands:
+            mpDraw.draw_landmarks(img, hand, mp.solutions.hands.HAND_CONNECTIONS)
+
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
