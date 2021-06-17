03f372c955636925c7452b877c80a74778e89394
Paul Pan <76851428+packetsss@users.noreply.github.com>
2021-06-17 13:06:38 -0700

    renamed a few tutorials

--- Python Tutorial MediaPipe/1_hand_tracking.py
+++ Python Tutorial MediaPipe/1_hand_tracking.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import time
 
 
-class HandDetect():
+class HandDetect:
     def __init__(self, mode=False, max_hands=2, min_detection_conf=0.5, min_tracking_conf=0.5):
         self.mode, self.max_hands, self.min_detection_conf, self.min_tracking_conf = \
             mode, max_hands, min_detection_conf, min_tracking_conf
@@ -15,21 +15,31 @@ class HandDetect():
 
     def find_hands(self, img, draw=True):
         rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        rst = self.mpHands.process(rgb)
-        if (hands := rst.multi_hand_landmarks) is not None:
-            for hand in hands:
-                # for i, landmark in enumerate(hand.landmark):
-                #     # print(i, landmark) # 21 coord info
-                #     h, w, ch = img.shape
-                #     cx, cy = int(landmark.x * w), int(landmark.y * h) # all 21 values
+        self.rst = self.mpHands.process(rgb)
 
-                #     if i == 0:
-                #         cv2.circle(img, (cx, cy), 5, (255, 0, 255), 15, cv2.FILLED)
+        if (hands := self.rst.multi_hand_landmarks) is not None:
+            for hand in hands:
                 if draw:
                     self.mpDraw.draw_landmarks(img, hand, mp.solutions.hands.HAND_CONNECTIONS)
+
+        return img
     
-    def find_pos(self, img):
-        pass
+    def find_position(self, img, hand=0, draw=True):
+        lm_list = []
+        if self.rst.multi_hand_landmarks is not None:
+            hand = self.rst.multi_hand_landmarks[hand]
+            for i, landmark in enumerate(hand.landmark):
+                    h, w, ch = img.shape
+                    cx, cy = int(landmark.x * w), int(landmark.y * h) # all 21 values
+
+                    lm_list.append((i, cx, cy))
+
+                    if draw:
+                        cv2.circle(img, (cx, cy), 5, (255, 0, 255), 5, cv2.FILLED)
+        
+        return lm_list
+
+
 
 def main():
     det = HandDetect()
@@ -39,8 +49,8 @@ def main():
 
     while 1:
         success, img = cap.read()
-        det.find_hands(img)
-        
+        img = det.find_hands(img)
+        lst = det.find_position(img)
 
         curr_time = time.time()
         fps = 1 / (curr_time - prev_time)
@@ -48,7 +58,8 @@ def main():
 
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Img", img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 
 if __name__ == '__main__':
