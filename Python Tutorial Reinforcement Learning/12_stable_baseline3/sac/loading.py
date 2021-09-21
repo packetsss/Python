0709f84812e9244df2bef38297190a388d09b751
Paul Pan <76851428+packetsss@users.noreply.github.com>
2021-09-21 10:49:21 -0700

    updated sb3 tutorials

---
+++ Python Tutorial Reinforcement Learning/12_stable_baseline3/sac/loading.py
@@ -0,0 +1,29 @@
+import gym
+import time
+import numpy as np
+from stable_baselines3 import SAC
+
+env = gym.make("BipedalWalker-v3")
+
+
+model = SAC.load(
+    "test/sac",
+    env=env,
+    policy="MlpPolicy",
+    device="cuda",
+    buffer_size=500000,
+    learning_starts=10000,
+    batch_size=1000,
+    optimize_memory_usage=True,
+    verbose=2,
+)
+
+
+obs = env.reset()
+for i in range(2000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        obs = env.reset()
+    time.sleep(0.01)
